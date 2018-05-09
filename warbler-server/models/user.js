b0d6d72ffe59248ff0a9340a076182d46a5e1455
neumann11 <pavlo.vlasyuk@gmail.com>
2018-05-09 13:02:13 +0200

    Add user middleware for password hashing and comparison

--- warbler-server/models/user.js
+++ warbler-server/models/user.js
@@ -21,6 +21,32 @@ const userSchema = new mongoose.Schema({
 	}
 });
 
+//Add pre-save hooks->hash passw. before user saved to db:
+userSchema.pre("save", async function(next){ //run func. before sav e
+	try{
+		if(!this.isModified("password"){
+			return next();
+		}
+		//async action:
+		let hashedPassword = await bcrypt.hash(this.password, 10); //10-salting, put addit. info into hash -> diff. hashes for same passw.
+		this.Password = hashedPassword;
+		return next(); //saving the doc. w. hashed passw. 
+	} catch(err){
+		return next(err); //-> goes to errHandler;
+	}
+});
+
+//async passw. comparison func
+//every doc. made from user model can compare another hashed passw. with their passw in db. To check if user put right passw.
+userSchema.method.comparePassword = async function(candidatePassword, next){
+	try {
+		let isMatch = await bcrypt.compare(candidatePassword, this.password); //compares passw from the form to encr. passw. in db
+		return isMatch; //returns true or false;
+	} catch(err) {
+		return next(err);
+	}
+};
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
