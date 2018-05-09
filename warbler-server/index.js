fb4e345c7528a7ead0d327061841dc63b15103c4
neumann11 <pavlo.vlasyuk@gmail.com>
2018-05-09 12:06:52 +0200

    First commit, initialize server index.js, install server-side dependancies

---
+++ warbler-server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// all routes:
+
+// Error handling if non of the routes is reached:
+app.use(function(req, res, next){
+	let err = new Error("Not Found") //Error is a built-in const func. in JS
+	err.status = 404;
+	next(err);
+});
+
+app.listen(PORT, function(){
+	console.log(`Server is starting on port ${PORT}`);
+});
