62fc6c6e57ff1cd324c863583e9b4fd6903a7c79
neumann11 <pavlo.vlasyuk@gmail.com>
2018-05-09 12:33:42 +0200

    Add User model

--- warbler-server/handlers/error.js
+++ warbler-server/handlers/error.js
@@ -1,4 +1,6 @@
 //Middleware to use after 404 errHandler:
+//Standard err. handler sends JSON
+
 function errorHandler(error, request, response, next){
 	//500: Route found but smt. wrong on server:
 	return response.status(error.status || 500).json({
---
+++ warbler-server/models/index.js
@@ -0,0 +1,11 @@
+// Connecting to mongodb and settingup mongoose;
+
+const mongoose = require("mongoose");
+mongoose.set("debug", true); //show mongo queries in the terminal
+mongoose.Promise = Promise; //specif. using ES15/17 Promises instead of using callbacks. Makes mongo methods return promises;
+mongoose.connect("mongodb://localhost/warbler", { //connect to mongodb
+	keepAlive: true,
+	useMongoClient: true
+});
+
+
