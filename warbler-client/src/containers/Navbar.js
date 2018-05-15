6e65531f07321f0b6e0b2a85f6444ecc08f4f269
neumann11 <pavlo.vlasyuk@gmail.com>
2018-05-15 10:16:12 +0200

    Add some styling

--- warbler-client/src/containers/Navbar.js
+++ warbler-client/src/containers/Navbar.js
@@ -8,9 +8,9 @@ class Navbar extends Component {
   logout = e => {
     e.preventDefault();
     this.props.logout();
-  }
+  };
   render() {
-    return(
+    return (
       <nav className="navbar navbar-expand">
         <div className="container-fluid">
           <div className="navbar-header">
@@ -20,11 +20,11 @@ class Navbar extends Component {
           </div>
 
           {this.props.currentUser.isAuthenticated ? (
-            <ul className="nav-navbar-nav navbar-right">
+            <ul className="nav navbar-nav navbar-right">
               <li>
                 <Link
                   to={`/users/${this.props.currentUser.user.id}/messages/new`}
-                  >
+                >
                   New Message
                 </Link>
               </li>
@@ -32,27 +32,28 @@ class Navbar extends Component {
                 <a onClick={this.logout}>Log out</a>
               </li>
             </ul>
-            )
-          : (
-          <ul className="nav navbar-nav navbar-right">
-            <li>
-              <Link to="/signup">Sign up</Link>
-            </li>
-            <li>
-              <Link to="/signin">Log in</Link>
-            </li>
-          </ul>
-        )}
+          ) : (
+            <ul className="nav navbar-nav navbar-right">
+              <li>
+                <Link to="/signup">Sign up</Link>
+              </li>
+              <li>
+                <Link to="/signin">Log in</Link>
+              </li>
+            </ul>
+          )}
         </div>
       </nav>
-    )
+    );
   }
 }
 
-function mapStateToProps(state){ //pass state from Redux state
-  return { //return an obj
+function mapStateToProps(state) {
+  //pass state from Redux state
+  return {
+    //return an obj
     currentUser: state.currentUser //currUser placed of props. Value taken from State
   };
 }
 
-export default connect(mapStateToProps, {logout})(Navbar);
+export default connect(mapStateToProps, { logout })(Navbar);
